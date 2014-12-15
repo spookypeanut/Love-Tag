e0c7444ab9f77401602c00ab35699d4c64eb2f10
spookypeanut <github@spookypeanut.co.uk>
2014-12-15 22:03:08 +0000

    Remove unused imports

--- app/src/main/java/uk/co/spookypeanut/lovetag/Love.java
+++ app/src/main/java/uk/co/spookypeanut/lovetag/Love.java
@@ -4,8 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.media.MediaMetadataRetriever;
-import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,9 +12,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-import java.io.File;
-
 
 public class Love extends ActionBarActivity {
 
