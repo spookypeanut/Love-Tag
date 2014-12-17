099964d1cd60a8cbccf77b05cf9d08b88a5352b2
spookypeanut <github@spookypeanut.co.uk>
2014-12-17 23:10:06 +0000

    Cleanup

--- app/src/main/java/uk/co/spookypeanut/lovetag/UrlMaker.java
+++ app/src/main/java/uk/co/spookypeanut/lovetag/UrlMaker.java
@@ -45,7 +45,6 @@ public class UrlMaker {
     private String generate_api_sig(Map<String, String> params) {
         List<String> keys = asSortedList(params.keySet());
         String secret = mContext.getString(R.string.lastfm_api_secret);
-        Log.i("Love&Tag.UrlMaker", secret);
         String pre_md5 = "";
         for (String key : keys) {
             String value = (String) params.get(key);
@@ -54,12 +53,11 @@ public class UrlMaker {
             }
             pre_md5 += key;
             pre_md5 += value;
-            Log.i("Love&Tag.UrlMaker", key + " = " + value);
         }
         pre_md5 += secret;
-        Log.i("Love&Tag.UrlMaker", "pre_md5: " + pre_md5);
+        Log.d("Love&Tag.UrlMaker", "pre_md5: " + pre_md5);
         String api_sig = mMd5Maker.encode(pre_md5);
-        Log.i("Love&Tag.UrlMaker", "api_sig: " + api_sig);
+        Log.d("Love&Tag.UrlMaker", "api_sig: " + api_sig);
         return api_sig;
     }
     public static <T extends Comparable<? super T>> List<T> asSortedList(Collection<T> c) {
