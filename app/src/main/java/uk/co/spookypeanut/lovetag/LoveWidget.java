83880d611eccd3493fc7cec4535f966fdd38f858
spookypeanut <github@spookypeanut.co.uk>
2015-02-20 23:50:48 +0000

    Do something when the button is clicked

--- app/src/main/java/uk/co/spookypeanut/lovetag/LoveWidget.java
+++ app/src/main/java/uk/co/spookypeanut/lovetag/LoveWidget.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.RemoteViews;
@@ -16,9 +17,25 @@ import android.widget.RemoteViews;
 public class LoveWidget extends AppWidgetProvider {
 
     @Override
+    public void onReceive(Context context, Intent intent) {
+        String tag = "Love&Tag.LoveWidget.onReceive";
+        if (intent.getAction()==null) {
+            Log.d(tag, "null action");
+            context.startService(new Intent(context, UpdateService.class));
+        }
+        else {
+            Log.d(tag, "non-null action: " + intent.getAction());
+            super.onReceive(context, intent);
+        }
+    }
+    @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        String tag = "Love&Tag.LoveWidget.onUpdate";
+        Log.d(tag, "Starting");
         // There may be multiple widgets active, so update all of them
-        context.startService(new Intent(context, UpdateService.class));
+        Intent i = new Intent(context, UpdateService.class);
+        i.setAction("WHAT");
+        context.startService(i);
         /*
         final int N = appWidgetIds.length;
         for (int i = 0; i < N; i++) {
@@ -41,25 +58,35 @@ public class LoveWidget extends AppWidgetProvider {
         private SharedPreferences mPrefs;
         public UpdateService() {
             super("LoveWidget$UpdateService");
+            String tag = "Love&Tag.LoveWidget.UpdateService";
+            Log.d(tag, "Constructor");
         }
         @Override
         public void onCreate() {
             super.onCreate();
+            String tag = "Love&Tag.LoveWidget.UpdateService.onCreate";
+            Log.d(tag, "Starting");
             mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
         }
         @Override
         public void onHandleIntent(Intent intent) {
+            String tag = "Love&Tag.LoveWidget.UpdateService.onHandleIntent";
+            Log.d(tag, "Starting");
             ComponentName me = new ComponentName(this, LoveWidget.class);
             AppWidgetManager mgr = AppWidgetManager.getInstance(this);
             mgr.updateAppWidget(me, buildUpdate(this));
         }
         private RemoteViews buildUpdate(Context context) {
+            String tag = "Love&Tag.LoveWidget.UpdateService.buildUpdate";
+            Log.d(tag, "Starting");
             CharSequence widgetText = context.getString(R.string.appwidget_text);
             // Construct the RemoteViews object
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.love_widget);
             views.setTextViewText(R.id.loveWidgetLabel, widgetText);
             Intent i = new Intent(this, LoveWidget.class);
-            PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
+            i.setAction("button_clicked");
+
+            PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
             views.setOnClickPendingIntent(R.id.loveWidgetButton, pi);
 
             return views;
