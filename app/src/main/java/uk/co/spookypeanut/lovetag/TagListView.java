76dd3f36cdc84a3d4faf104ed6c1a6429c831dab
spookypeanut <github@spookypeanut.co.uk>
2015-02-28 21:34:00 +0000

    Extend ListView to make TagListView

---
+++ app/src/main/java/uk/co/spookypeanut/lovetag/TagListView.java
@@ -0,0 +1,11 @@
+package uk.co.spookypeanut.lovetag;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+public class TagListView extends ListView {
+      public TagListView(Context context, AttributeSet attrs) {
+          super(context, attrs);
+      }
+}
