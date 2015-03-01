5b28a219929dfb441179648d89b334d64b7e01fe
spookypeanut <github@spookypeanut.co.uk>
2015-03-01 23:11:14 +0000

    Make custom View more efficient

--- app/src/main/java/uk/co/spookypeanut/lovetag/TagListEntryView.java
+++ app/src/main/java/uk/co/spookypeanut/lovetag/TagListEntryView.java
@@ -11,12 +11,51 @@ import android.util.Log;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TagListEntryView extends TextView {
+    List<Point> mPointList = new ArrayList<>();
+    Paint mActiveFillPaint = new Paint();
+    Paint mInactiveFillPaint = new Paint();
+    Paint mActiveDrawPaint = new Paint();
+    Paint mInactiveDrawPaint = new Paint();
 
     public TagListEntryView(Context context, AttributeSet attrs) {
         super(context, attrs);
         String tag = "Love&Tag.TagListEntryView";
         Log.d(tag, "Constructor");
+        init();
+    }
+
+    private void init() {
+        Resources r = getResources();
+        int col_ref;
+        col_ref = R.color.tag_active_background;
+        mActiveFillPaint.setColor(r.getColor(col_ref));
+        col_ref = R.color.tag_inactive_background;
+        mInactiveFillPaint.setColor(r.getColor(col_ref));
+        col_ref = R.color.tag_active_line;
+        mActiveDrawPaint.setColor(r.getColor(col_ref));
+        col_ref = R.color.tag_inactive_line;
+        mInactiveDrawPaint.setColor(r.getColor(col_ref));
+    }
+
+    @Override
+    protected void onSizeChanged(int width, int height, int old_w, int old_h) {
+        int top = 0;
+        int bottom = height;
+        int left = 0;
+        int right = width;
+
+        int pointHeight = (int) (width * 0.1);
+
+        mPointList.add(new Point(left, top));
+        mPointList.add(new Point(right - pointHeight, top));
+        mPointList.add(new Point(right, top + height / 2));
+        mPointList.add(new Point(right - pointHeight, bottom));
+        mPointList.add(new Point(left, bottom));
+
     }
 
     @Override
@@ -28,45 +67,21 @@ public class TagListEntryView extends TextView {
     private void drawBackground(Canvas canvas) {
         String tag = "Love&Tag.TagListEntryView.drawBackground";
         Log.d(tag, "Starting");
-        double xScale = 0.95;
-        double yScale = 0.9;
-
-        int height = getHeight();
-        int width = getWidth();
-        int top = 0;
-        int bottom = height;
-        int left = 0;
-        int right = width;
-
-        int pointHeight = (int) (width * 0.1);
-
-        Point a = new Point(left, top);
-        Point b = new Point(right - pointHeight, top);
-        Point c = new Point(right, top + height / 2);
-        Point d = new Point(right - pointHeight, bottom);
-        Point e = new Point(left, bottom);
 
         Path path = new Path();
-        path.moveTo(a.x, a.y);
-        path.lineTo(b.x, b.y);
-        path.lineTo(c.x, c.y);
-        path.lineTo(d.x, d.y);
-        path.lineTo(e.x, e.y);
 
-        Paint fill_paint = new Paint();
-        Paint line_paint = new Paint();
-        int fill_ref;
-        int line_ref;
-        if (true) {
-            fill_ref = R.color.tag_active_background;
-            line_ref = R.color.tag_active_line;
-        } else {
-            fill_ref = R.color.tag_inactive_background;
-            line_ref = R.color.tag_inactive_line;
+        Point p;
+        p = mPointList.get(0);
+        path.moveTo(p.x, p.y);
+        for (int index=1; index<mPointList.size(); index++) {
+            p = mPointList.get(index);
+            path.lineTo(p.x, p.y);
         }
-        Resources R = getResources();
-        fill_paint.setColor(R.getColor(fill_ref));
-        line_paint.setColor(R.getColor(line_ref));
-        canvas.drawPath(path, fill_paint);
+        // TODO: do active detection here
+        if (true) {
+            canvas.drawPath(path, mActiveFillPaint);
+        } else {
+            canvas.drawPath(path, mInactiveFillPaint);
+        }
     }
 }
