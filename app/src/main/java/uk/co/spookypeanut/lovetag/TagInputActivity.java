b48c4e9ca1825ae4c3b8579c0ef541d88cbf8bba
spookypeanut <github@spookypeanut.co.uk>
2015-03-15 00:05:33 +0000

    Remove unused import

--- app/src/main/java/uk/co/spookypeanut/lovetag/TagInputActivity.java
+++ app/src/main/java/uk/co/spookypeanut/lovetag/TagInputActivity.java
@@ -28,7 +28,6 @@ import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ListView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
